362ff80420b0fb33472f0885b984545f18c03f27
ikhlas5 <mohammadikhlas765@gmail.com>
2022-10-11 12:06:29 +0600

    third commit

--- src/componetns/ProductDetails/ProductDetails.js
+++ src/componetns/ProductDetails/ProductDetails.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Quiz from '../Quiz/Quiz';
 import'./ProductDetails.css'
 const ProductDetails = () => {
-    const productDetails=useLoaderData();
+    const productDetails=useLoaderData().data.questions;
     console.log(productDetails);
-    const{question}=productDetails.data.questions;
+    const{question}=productDetails;
+    console.log(question);
     return (
         <div>
             <h1>Quiz:{question}</h1>
+            {
+                productDetails.map(productDetail=><Quiz  
+                key={productDetail.id} 
+                productDetail={productDetail}>
+                </Quiz>)
+            }
         </div>
     );
 };
---
+++ src/componetns/Quiz/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quiz = ({productDetail}) => {
+    console.log(productDetail)
+    return (
+        <div>
+            <h1>quiz</h1>
+        </div>
+    );
+};
+
+export default Quiz;
