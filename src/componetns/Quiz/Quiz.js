2d1cb5b263362446b404ebfe625970038850ce0d
ikhlas5 <mohammadikhlas765@gmail.com>
2022-10-11 21:53:05 +0600

    ten commit

--- src/componetns/Quiz/Quiz.js
+++ src/componetns/Quiz/Quiz.js
@@ -17,21 +17,30 @@ const Quiz = ({productDetail}) => {
         }
     }
     return (
-        <div className=' bg-slate-500 m-5'>
+        <div className=' bg-indigo-300 m-5  rounded shadow-2xl'>
             <h1 className='text-3xl'>Quiz Qusation</h1>
             <div className=''>
                 
             <h1>Questions:{question}</h1>
-            {
-                options.map(option=>
-                    <button onClick={()=>checkAnswer(option)} className=''>
-                        <input type='radio' id={id} name='fav_language' value='HTML'></input>
-                        <label for={id} >{option}</label>
-                    </button>
-                    )
-            }
-            <ToastContainer></ToastContainer>
+            
+                <div className='grid grid-cols-2 m-10'>
+
+                   {
+                      options.map(option=>
+                        <button onClick={()=>checkAnswer(option)} className=''>
+                            <div className='flex items-center mx-auto p-4 rounded-xl bg-gray-300 mb-5 mr-5 '>
+                         <CheckCircleIcon className="h-4 w-6 text-stone-800">
+                         </CheckCircleIcon>
+                            <label className=' m-5' for={id} >{option}</label>
+                            </div>
+                        </button>
+                        )
+                     }
+                </div>
+            
+            
             </div>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
